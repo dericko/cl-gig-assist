4c484204b6c9f5540c18ffcf2bcf0f5ff82e5443
Derick Olson <olsonderick@gmail.com>
2017-10-12 12:50:22 -0400

    add prettier precommit

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className="Form">
+          <div className="Form-Keywords" />
+          <div className="Form-Keywords" />
+        </div>
       </div>
     );
   }
